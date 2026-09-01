9558fbda328e5f2e38096ef2041e763ce7985302
seyed84s <seyedmmm84@gmail.com>
2026-09-01 23:04:26 +0330

    fix(core): add missing import for PsiphonRegion in ProfileStore

--- app/src/main/java/studio/cluvex/aether/data/ProfileStore.kt
+++ app/src/main/java/studio/cluvex/aether/data/ProfileStore.kt
@@ -14,6 +14,7 @@ import studio.cluvex.aether.model.EndpointMode
 import studio.cluvex.aether.model.IpVersion
 import studio.cluvex.aether.model.Noize
 import studio.cluvex.aether.model.Protocol
+import studio.cluvex.aether.model.PsiphonRegion
 import studio.cluvex.aether.model.ScanMode
 import studio.cluvex.aether.model.SplitMode
 import studio.cluvex.aether.model.TeamAuth
